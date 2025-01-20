72899303cc5d612b49dc45d6af850d8ba39f014b
Themike <magsa37p1@gmail.com>
2025-01-20 13:30:41 +0700

    V.01
    install vite-react-ts. tailwind
    config

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import image1 from './assets/react.svg'
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+   
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+    
+
+  )
+}
+
+export default App
